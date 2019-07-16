e73267cf1489f64a965ad5993b297d83344e2782
lzh-123 <709507253@qq.com>
2019-07-16 05:18:11 -0700

    --------

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+	printf("--------------------.\n")
+	printf("--------------------.\n")
+	printf("--------------------.\n")
+	printf("--------------------.\n")
+
+	return 0;
+}
